9a50e1ef002130fba7cf4f87d82bad31acde8d0f
jacksina <jacksina@iMac.local>
2024-03-25 11:09:16 +0300

    added meta tags

---
+++ src/components/Footer.js
@@ -0,0 +1,8 @@
+
+const Footer = ()=>{
+    return(
+        <h2>Little lemon restaurant</h2>
+    )
+}
+
+export default Footer
---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import logo from './../assets/Logo.svg'
+const Header = () =>{
+    return(
+        <>
+            <img src={logo} alt="little lemon logo"/>
+        </>
+    )
+}
+export default Header;
